61acfbd539d5b3f527a47e8d41822894542fa45d
Madi-D <63809763+Madi-D@users.noreply.github.com>
2026-03-25 15:49:12 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+/*
+PURPOSE: 
+INPUT:
+OUTPUT
+*/
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Student Score Merger</h1>
+    </div>
+  )
+}
+
+export default App
